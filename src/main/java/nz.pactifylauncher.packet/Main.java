07b80c4ee7aceff046435b84836bc67282c556d0
6myy <155362207+6myy@users.noreply.github.com>
2024-01-24 23:49:50 +0100

    Create Main.java

---
+++ src/main/java/nz.pactifylauncher.packet/Main.java
@@ -0,0 +1,14 @@
+package nz.pactifylauncher.packet;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
